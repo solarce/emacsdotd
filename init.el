3b1814c55b95922591f2a399e232ceb0ff0cea3e
Brandon Burton <brandon@inatree.org>
2014-05-21 08:39:25 -0700

    syntax highlight for rc files and some commentary

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+; let's get some ELPA repos setup so we can enhance all the things
 (require 'package)
 (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
@@ -5,6 +6,26 @@
 			 ;("marmalade" . "http://marmalade-repo.org/packages/")
 			 ))
 
+; solarized-dark errywhere
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
-
 (load-theme 'solarized-dark t)
+
+; handy for stuff like ~/.muttrc
+(add-to-list 'auto-mode-alist '("\\.*rc$" . conf-unix-mode))
+
+; ensures new gui clients end up as frames of a single gui instance
+(when (featurep 'ns)
+  (defun ns-raise-emacs ()
+    "Raise Emacs."
+    (ns-do-applescript "tell application \"Emacs\" to activate"))
+
+  (defun ns-raise-emacs-with-frame (frame)
+    "Raise Emacs and select the provided frame."
+    (with-selected-frame frame
+      (when (display-graphic-p)
+        (ns-raise-emacs))))
+
+  (add-hook 'after-make-frame-functions 'ns-raise-emacs-with-frame)
+
+  (when (display-graphic-p)
+    (ns-raise-emacs)))
